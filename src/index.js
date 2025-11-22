55f142dd8316962b2ea82dd6e5e8a6c182b2fdde
Mohd-Affan-Code <mohdaffanjkhan9@gmail.com>
2025-11-22 10:27:57 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
-
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runnning port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Monogo db connection failed !!!", error);
+  });
 
 // import express from "express";
 // const app = express();
